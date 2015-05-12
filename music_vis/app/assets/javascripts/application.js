435f925af277bde52c28e102a9778f6e5b3569f7
Phillip <phil.winston2@gmail.com>
2015-05-12 16:32:31 -0400

    added oscilloscope to learn web audio api

--- music_vis/app/assets/javascripts/application.js
+++ music_vis/app/assets/javascripts/application.js
@@ -18,13 +18,65 @@
 $('document').ready(function() {
 
   var button = document.getElementById('start');
-  var sound = new Howl({urls: ['/app/assets/javascripts/dm.mp3']});
+  var canvas = document.getElementById("canvas");
+  var ctx = canvas.getContext("2d");
+  var width = canvas.width;
+  var height = canvas.height;
 
-  button.addEventListener('click', function(event){
-    event.preventDefault();
-    alert('hi');
-    sound.play();
-  });
+  var audio = new Audio();
+  audio.src = 'dawn.mp3';
+  audio.controls = true;
+  document.body.appendChild(audio);
 
+  var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  var analyser = audioCtx.createAnalyser();
+  var source = audioCtx.createMediaElementSource(audio);
+  source.connect(analyser);
+  analyser.connect(audioCtx.destination);
+
+  analyser.fftSize = 2048;
+  var bufferLength = analyser.frequencyBinCount;
+  var dataArray = new Uint8Array(bufferLength);
+  analyser.getByteTimeDomainData(dataArray);
+  ctx.clearRect(0, 0, width, height);
+
+
+
+  function draw() {
+    var drawVisual = requestAnimationFrame(draw);
+    analyser.getByteTimeDomainData(dataArray);
+    ctx.fillStyle = 'rgb(200, 200, 200)';
+    ctx.fillRect(0, 0, width, height);
+    ctx.lineWidth = 2;
+    ctx.strokeStyle = 'rgb(0, 0, 0)';
+    ctx.beginPath();
+    var sliceWidth = width * 1.0 / bufferLength;
+    var x = 0;
+      for(var i = 0; i < bufferLength; i++) {
+        var v = dataArray[i] / 128.0;
+        var y = v * height/2;
+        if(i === 0) {
+          ctx.moveTo(x, y);
+        } else {
+          ctx.lineTo(x, y);
+        }
+        x += sliceWidth;
+      }
+      ctx.lineTo(width, height/2);
+      ctx.stroke();
+    }
+  draw();
 
 });
+
+  // var sound = new Howl({
+  //   urls: ['dawn.mp3']
+  // });
+
+
+  // button.addEventListener('click', function(event){
+  //   event.preventDefault();
+  //
+  //   // currentTrack.attr('src','dawn.mp3')
+  //   sound.play();
+  // });
