0df97ffeaf0937d1d10704639fe21710c4a1b685
Phillip <phil.winston2@gmail.com>
2015-05-14 13:49:26 -0400

    took out api, added artist info render

--- music_vis/app/assets/javascripts/application.js
+++ music_vis/app/assets/javascripts/application.js
@@ -23,26 +23,31 @@ $('document').ready(function() {
   // }).done(function(response){
   //   console.log(response)
   // });
+  var artistName = document.getElementById('artist_name');
+  var artistBio = document.getElementById('artist_bio');
 
-  var newartist = document.getElementById('newartist')
+  var newartist = document.getElementById('newartist');
   var button = document.getElementById('start');
   var canvas = document.getElementById('canvas');
   var canvasContext = canvas.getContext('2d');
   var width = canvas.width;
   var height = canvas.height;
 
+
   newartist.addEventListener('click', function(event){
     event.preventDefault();
     $.ajax({
       type: "GET",
       dataType: "json",
-      url: window.location.origin + "/artists.json"
+      url: window.location.origin  + "/artists.json"
     }).done(function(response){
       console.log(response);
       for(var i = 0; i < response.length; i++)
       {
         if(response[i].name === newartist.innerHTML){
           audio.src = 'audios/' + response[i].audio_id;
+          artistName.innerHTML = response[i].name;
+          artistBio.innerHTML = response[i].bio;
         }
       }
     });
@@ -61,61 +66,79 @@ $('document').ready(function() {
   analyser.connect(audioCtx.destination);
 
 // **oscilloscope viz*
-  // function scope() {
-  //   analyser.fftSize = 2048;
-  //   var bufferLength = analyser.frequencyBinCount;
-  //   var dataArray = new Uint8Array(bufferLength);
-  //   analyser.getByteTimeDomainData(dataArray);
-  //
-  //   var drawVisual = requestAnimationFrame(scope);
-  //   canvasContext.clearRect(0, 0, width, height);
-  //   canvasContext.fillStyle = 'rgb(200, 200, 200)';
-  //   canvasContext.fillRect(0, 0, width, height);
-  //   canvasContext.lineWidth = 2;
-  //   canvasContext.strokeStyle = 'rgb(0, 0, 0)';
-  //   canvasContext.beginPath();
-  //
-  //   var sliceWidth = width * 1.0 / bufferLength;
-  //   var x = 0;
-  //     for(var i = 0; i < bufferLength; i++) {
-  //       var v = dataArray[i] / 128.0;
-  //       var y = v * height/2;
-  //       if(i === 0) {
-  //         canvasContext.moveTo(x, y);
-  //       } else {
-  //         canvasContext.lineTo(x, y);
-  //       }
-  //       x += sliceWidth;
-  //     }
-  //     canvasContext.lineTo(width, height/2);
-  //     canvasContext.stroke();
-  //   }
-  //   scope();
-
-
-  function bars() {
-    analyser.fftSize = 256;
+  function scope() {
+    analyser.fftSize = 2048;
     var bufferLength = analyser.frequencyBinCount;
     var dataArray = new Uint8Array(bufferLength);
-    analyser.getByteFrequencyData(dataArray);
+    analyser.getByteTimeDomainData(dataArray);
 
-    var drawVisual = requestAnimationFrame(bars);
+
+    var drawVisual = requestAnimationFrame(scope);
     canvasContext.clearRect(0, 0, width, height);
-    canvasContext.fillStyle = 'rgb(0,0,0)';
+    canvasContext.fillStyle = 'rgb(255, 255, 255)';
     canvasContext.fillRect(0, 0, width, height);
+    canvasContext.lineWidth = 2;
+    canvasContext.strokeStyle = 'rgb(0, 0, 0)';
+    canvasContext.beginPath();
 
-    var barWidth = (width / bufferLength);
-    // ^ * 2.5 as baseline
-    var barHeight;
+    var sliceWidth = width / bufferLength;
     var x = 0;
       for(var i = 0; i < bufferLength; i++) {
-        barHeight = dataArray[i];
-        canvasContext.fillStyle = 'rgb(' + (barHeight+100) + ', 100, 50)';
-        canvasContext.fillRect(x, height-barHeight/2, barWidth, barHeight);
-        x += barWidth + 1;
+        var v = dataArray[i] / 128.0;
+        var y = v * height/2;
+        if(i === 0) {
+          canvasContext.moveTo(x, y);
+        } else {
+          canvasContext.lineTo(x, y);
+        }
+        x += sliceWidth;
       }
-    }
+      canvasContext.lineTo(width, height/2);
+      canvasContext.stroke();
 
-    bars();
+      // analyser.fftSize = 512;
+        var bufferLength = analyser.frequencyBinCount;
+        var dataArray = new Uint8Array(bufferLength);
+        analyser.getByteFrequencyData(dataArray);
+
+      var barWidth = (width / bufferLength);
+      // ^ * 2.5 as baseline
+      var barHeight;
+      var x = 0;
+        for(var i = 0; i < bufferLength; i++) {
+          barHeight = dataArray[i];
+          canvasContext.fillStyle = 'rgb(' + (barHeight+100) + ', 100, 50)';
+          canvasContext.fillRect(x, height-barHeight/2, barWidth, barHeight);
+          x += barWidth + 1;
+        }
+
+    }
+  scope();
+
+
+  // function bars() {
+  //   analyser.fftSize = 512;
+  //   var bufferLength = analyser.frequencyBinCount;
+  //   var dataArray = new Uint8Array(bufferLength);
+  //   analyser.getByteFrequencyData(dataArray);
+  //
+  //   var drawVisual = requestAnimationFrame(bars);
+  //   canvasContext.clearRect(0, 0, width, height);
+  //   canvasContext.fillStyle = 'rgb(255, 255,255)';
+  //   canvasContext.fillRect(0, 0, width, height);
+  //
+  //   var barWidth = (width / bufferLength);
+  //   // ^ * 2.5 as baseline
+  //   var barHeight;
+  //   var x = 0;
+  //     for(var i = 0; i < bufferLength; i++) {
+  //       barHeight = dataArray[i];
+  //       canvasContext.fillStyle = 'rgb(' + (barHeight+100) + ', 100, 50)';
+  //       canvasContext.fillRect(x, height-barHeight/2, barWidth, barHeight);
+  //       x += barWidth + 1;
+  //     }
+  //   }
+  //
+  //   bars();
 
 });
